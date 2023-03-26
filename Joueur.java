8169b9cae676ddf364b811825ffe9f583699215b
KillianVR <94352991+Kirua6@users.noreply.github.com>
2023-03-26 21:58:05 +0200

    Add files via upload

---
+++ Joueur.java
@@ -0,0 +1,77 @@
+
+package est.grand.Yoda;
+
+import est.grand.Commons;
+
+import javax.swing.*;
+import java.awt.event.KeyEvent;
+
+public class Joueur extends Papa_Noël {
+
+    private int width;
+
+    public Joueur() {
+
+        initJoueur();
+    }
+
+    private void initJoueur() {
+
+        var playerImg = "src/images/player.png";
+        var ii = new ImageIcon(playerImg);
+
+        width = ii.getImage().getWidth(null);
+        setImage(ii.getImage());
+
+        int START_X = 270;
+        setX(START_X);
+
+        int START_Y = 280;
+        setY(START_Y);
+    }
+
+    public void act() {
+
+        x += dx;
+
+        if (x <= 2) {
+
+            x = 2;
+        }
+
+        if (x >= Commons.BOARD_WIDTH - 2 * width) {
+
+            x = Commons.BOARD_WIDTH - 2 * width;
+        }
+    }
+
+    public void keyPressed(KeyEvent e) {
+
+        int key = e.getKeyCode();
+
+        if (key == KeyEvent.VK_LEFT) {
+
+            dx = -2;
+        }
+
+        if (key == KeyEvent.VK_RIGHT) {
+
+            dx = 2;
+        }
+    }
+
+    public void keyReleased(KeyEvent e) {
+
+        int key = e.getKeyCode();
+
+        if (key == KeyEvent.VK_LEFT) {
+
+            dx = 0;
+        }
+
+        if (key == KeyEvent.VK_RIGHT) {
+
+            dx = 0;
+        }
+    }
+}
---
+++ Tir.java
@@ -0,0 +1,27 @@
+package est.grand.Yoda;
+
+import javax.swing.*;
+
+public class Tir extends Papa_Noël {
+
+    public Tir() {
+    }
+
+    public Tir(int x, int y) {
+
+        initShot(x, y);
+    }
+
+    private void initShot(int x, int y) {
+
+        var shotImg = "src/images/shot.png";
+        var ii = new ImageIcon(shotImg);
+        setImage(ii.getImage());
+
+        int H_SPACE = 6;
+        setX(x + H_SPACE);
+
+        int V_SPACE = 1;
+        setY(y - V_SPACE);
+    }
+}
